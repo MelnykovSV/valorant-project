396e533fb5bbbecf187bd242910f2a5551f8c5c3
MelnykovSV <svmelnykov@gmail.com>
2023-01-15 17:38:55 +0200

    agents

--- src/decoration.js
+++ src/decoration.js
@@ -1,62 +1,110 @@
 const sections = document.querySelectorAll('.decorated-section');
 
-[...sections].map(sec => {
-  // const section = document.querySelector('.decorated-section');
-  const container = sec.querySelector('.test-container');
-  const title = sec.querySelector('.test-h2');
-  const aside = sec.querySelector('.aside');
-  console.log(container, title, aside);
+console.log(window.innerWidth);
 
-  const sectionHeight = sec.offsetHeight;
+if (window.innerWidth >= 768) {
+  [...sections].map(sec => {
+    // ====================First pseudoelement==========================
 
-  const sectionPaddingTop = parseInt(
-    window.getComputedStyle(sec).getPropertyValue('padding-top')
-  );
+    // --------vertical--------
+    const container = sec.querySelector('.container');
+    const title = sec.querySelector('.decorated-title');
+    const aside = sec.querySelector('.decorated-aside');
+    console.log(container, title, aside);
 
-  const titleHeight = title.offsetHeight;
-  const deco1Height = sectionHeight - sectionPaddingTop - titleHeight;
-  sec.style.setProperty('--first-pseudoelement-height', deco1Height - 1 + 'px');
+    const sectionHeight = sec.offsetHeight;
 
-  sec.style.setProperty(
-    '--first-pseudoelement-left',
-    getComputedStyle(container).getPropertyValue('padding-left') + 'px'
-  );
+    const sectionPaddingTop = parseInt(
+      window.getComputedStyle(sec).getPropertyValue('padding-top')
+    );
 
-  // --------horisontal--------
+    const titleHeight = title.offsetHeight;
+    const deco1Height = sectionHeight - sectionPaddingTop - titleHeight;
+    sec.style.setProperty(
+      '--first-pseudoelement-height',
+      deco1Height - 1 + 'px'
+    );
 
-  sec.style.setProperty(
-    '--first-pseudoelement-width',
-    title.offsetWidth + 'px'
-  );
-  sec.style.setProperty('--first-pseudoelement-top', titleHeight + 'px');
+    sec.style.setProperty(
+      '--first-pseudoelement-left',
+      getComputedStyle(container).getPropertyValue('padding-left') + 'px'
+    );
 
-  // ====================Second pseudoelement==========================
+    // --------horisontal--------
 
-  sec.style.setProperty(
-    '--second-pseudoelement-right',
-    aside.offsetWidth / 3 +
-      parseInt(getComputedStyle(container).getPropertyValue('padding-right')) +
-      'px'
-  );
+    sec.style.setProperty(
+      '--first-pseudoelement-width',
+      title.offsetWidth + 'px'
+    );
+    sec.style.setProperty('--first-pseudoelement-top', titleHeight + 'px');
 
-  sec.style.setProperty(
-    '--second-pseudoelement-height',
-    sectionHeight -
-      aside.offsetHeight -
-      parseInt(getComputedStyle(aside).getPropertyValue('bottom')) -
-      10 +
-      'px'
-  );
+    // ====================Second pseudoelement==========================
 
-  sec.style.setProperty(
-    '--second-pseudoelement-top',
-    0 - sectionPaddingTop + 'px'
-  );
+    sec.style.setProperty(
+      '--second-pseudoelement-right',
+      aside.offsetWidth / 3 +
+        parseInt(
+          getComputedStyle(container).getPropertyValue('padding-right')
+        ) +
+        'px'
+    );
 
-  aside.style.right =
-    getComputedStyle(container).getPropertyValue('padding-right');
-});
+    sec.style.setProperty(
+      '--second-pseudoelement-height',
+      sectionHeight -
+        aside.offsetHeight -
+        parseInt(getComputedStyle(aside).getPropertyValue('bottom')) -
+        10 +
+        'px'
+    );
 
-// ====================First pseudoelement==========================
+    sec.style.setProperty(
+      '--second-pseudoelement-top',
+      0 - sectionPaddingTop + 'px'
+    );
 
-// --------vertical--------
+    aside.style.right =
+      getComputedStyle(container).getPropertyValue('padding-right');
+  });
+}
+
+// =================EXAMPLE OF SECTION TO BE DECORATED================
+
+{
+  /* <section class="section decorated-section">
+  <div class="container">
+    <h2 class="decorated-title">Title of random length fghjjfgjhgjjhgjhjhgj</h2>
+    <p class="test-p">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iure
+      pariatur a excepturi, velit incidunt perferendis illum, distinctio
+      delectus voluptatum reprehenderit ducimus dignissimos. Voluptas
+      voluptatibus velit adipisci amet quaerat omnis, sed esse. Illum nulla
+      blanditiis quidem eum laboriosam obcaecati aliquam. Modi laboriosam
+      voluptatum dignissimos sint distinctio incidunt? Amet quia molestias,
+      nulla enim ab dolor maiores dolores quisquam aspernatur, nesciunt
+      assumenda labore quidem deserunt vero accusamus, eum debitis voluptates.
+      Dolore aliquam, est tempore corrupti libero a nihil, non cumque neque enim
+      doloremque rerum sed possimus? Ipsa inventore, ea pariatur dolores omnis
+      labore optio accusantium aspernatur natus at incidunt magni quidem qui
+      impedit praesentium debitis odit. Ut, magni officia corrupti accusamus
+      omnis sint mollitia cupiditate velit, tenetur laborum ducimus repellat
+      libero, expedita provident impedit nihil tempore delectus! Veritatis, nemo
+      eius voluptatibus modi quibusdam, voluptatem, non vero id adipisci quos
+      veniam fugiat deserunt! Eligendi excepturi illo corporis harum aut unde
+      blanditiis provident optio, molestias natus ipsum exercitationem cumque
+      autem quibusdam quos omnis soluta debitis minima velit ducimus est,
+      voluptate pariatur facere temporibus? Non est, deserunt velit voluptates
+      nihil, odit inventore excepturi ea quasi adipisci maiores animi? Porro
+      illum enim sit ex necessitatibus, eos impedit tempora facere rerum ducimus
+      veritatis voluptatibus tempore fugiat repudiandae consequuntur, quos
+      assumenda debitis reprehenderit quam eveniet vero et! Iusto, commodi?
+      Expedita laudantium rerum suscipit perspiciatis maxime, omnis excepturi
+      ipsum voluptates eum tenetur voluptatum. Porro atque blanditiis cum
+      tempora vero! Culpa possimus ullam porro iure minima, obcaecati eum
+    </p>
+  
+
+    <p class="decorated-aside">Lorem ipsum dolor</p>
+  </div>
+</section> */
+}
