f2f755e0a4bc36b46f69dc1ee6ae1b4e3f5a92eb
MelnykovSV <svmelnykov@gmail.com>
2023-01-15 22:36:08 +0200

    cutting script + fixed gradient done

--- src/cutting.js
+++ src/cutting.js
@@ -1,6 +1,11 @@
 addEventListener('resize', () => {
   const sections = document.querySelectorAll('.cut');
+
   [...sections].map(sec => {
-    const container = sec.querySelector('container-new');
+    const container = sec.querySelector('.container-new');
+    const cutElementWidth =
+      (document.body.clientWidth - container.offsetWidth) / 2 +
+      parseInt(getComputedStyle(container).getPropertyValue('padding-right'));
+    sec.style.setProperty('--cutting-element-width', cutElementWidth + 'px');
   });
 });
