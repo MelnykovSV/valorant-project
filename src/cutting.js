be05ac2887416f16dcd852ce18398a6e66e23dce
MelnykovSV <svmelnykov@gmail.com>
2023-01-15 20:22:01 +0200

    cutting script

---
+++ src/cutting.js
@@ -0,0 +1,6 @@
+addEventListener('resize', () => {
+  const sections = document.querySelectorAll('.cut');
+  [...sections].map(sec => {
+    const container = sec.querySelector('container-new');
+  });
+});
